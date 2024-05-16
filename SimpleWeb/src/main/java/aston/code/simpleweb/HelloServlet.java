86390a4d533ac69feff08d911bceeb3e6342adaf
Vladislav Pomozov <vpactually@gmail.com>
2024-05-16 17:35:26 +0300

    new msg

--- SimpleWeb/src/main/java/aston/code/simpleweb/HelloServlet.java
+++ SimpleWeb/src/main/java/aston/code/simpleweb/HelloServlet.java
@@ -6,13 +6,13 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message = "Hello World!";
+    private String message = "Hello World!\n";
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "New World!!!" + "</h1>");
+        out.println("<h1>" + message +  + "</h1>");
         out.println("</body></html>");
     }
 
